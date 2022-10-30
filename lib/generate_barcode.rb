f3f8a786c35e23f05cc30cde5c6f0e0e01b241db
Sheharyar <mindsublimes@gmail.com>
2022-10-31 00:13:38 +0500

    Added new method for generating barcode

--- lib/generate_barcode.rb
+++ lib/generate_barcode.rb
@@ -10,12 +10,7 @@ class GenerateBarcode
     even_characters = get_even_characters(characters)
     odd_characters = get_odd_characters(characters)
     total_sum = get_total_sum(even_characters, odd_characters)
-    number = 10 - total_sum.modulo(10)
-    if number == 10
-      puts characters.push(0).join
-    else
-      puts characters.push(number).join
-    end
+    generate_isbn_13_barcode(characters, total_sum)
   end
 
   def get_even_characters(chars)
@@ -28,5 +23,14 @@ class GenerateBarcode
 
   def get_total_sum(even_chars, odd_chars)
     even_chars.map{|n| n.to_i}.sum + odd_chars.map{|n| n.to_i * 3}.sum
+  end
+
+  def generate_isbn_13_barcode(characters, total_sum)
+    number = 10 - total_sum.modulo(10)
+    if number == 10
+      puts characters.push(0).join
+    else
+      puts characters.push(number).join
+    end
   end
 end
