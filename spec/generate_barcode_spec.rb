aa1da24db97597853888de82366660e781e01a6c
Sheharyar <mindsublimes@gmail.com>
2022-10-31 00:05:22 +0500

    Added test cases

--- spec/generate_barcode_spec.rb
+++ spec/generate_barcode_spec.rb
@@ -2,4 +2,18 @@ require 'pry'
 require './lib/generate_barcode' 
 
 describe GenerateBarcode do
+  describe "#generate barcode" do
+    it "if the end number is not 10" do
+      expect do
+        GenerateBarcode.new("978014300723").create
+
+      end.to output("9780143007234\n").to_stdout
+    end
+
+    it "if the end number is 10" do
+      expect do
+        GenerateBarcode.new("978014300721").create
+      end.to output("9780143007210\n").to_stdout
+    end
+  end
 end
